c31b17bd2b95d216f09477d67cdc522c7166a55c
SteveHere <13937978+SteveHere@users.noreply.github.com>
2024-04-11 01:55:54 +0800

    flexbox attempt 2

---
+++ public/bwip-js_files/barcodeDefaults.js
@@ -0,0 +1,9 @@
+window.barcodeDefaults = {
+
+};
+
+// This function checks if the input data is valid for the given barcode format
+window.barcodeDataValidityChecker = () => {
+    
+};
+
--- public/bwip-js_files/drawing-svg.js
+++ public/bwip-js_files/drawing-svg.js
@@ -25,12 +25,13 @@
 }(typeof self !== 'undefined' ? self : this, function () {
     "use strict";
 
-    function DrawingSVG(opts, FontLib, integers_only=false) {
+    function DrawingSVG(opts, FontLib, integers_only=false, textScale={width:1, height:1}, textOffset={x:0, y:0}) {
+        let rot = 'N';
         // Unrolled x,y rotate/translate matrix
         var tx0 = 0, tx1 = 0, tx2 = 0, tx3 = 0;
         var ty0 = 0, ty1 = 0, ty2 = 0, ty3 = 0;
 
-        var svg = '';
+        let svg = [];
         var path;
         var lines = {};
 
@@ -40,6 +41,8 @@
         // Global graphics state
         var gs_width, gs_height;    // image size, in pixels
         var gs_dx, gs_dy;           // x,y translate (padding)
+
+        let glyph_defs = [];
 
         return {
             // Make no adjustments
@@ -52,8 +55,7 @@
             // `fwidth` and `fheight` are the requested font cell size.  They will
             // usually be the same, except when the scaling is not symetric.
             measure(str, font, fwidth, fheight) {
-                fwidth = fwidth|0;
-                fheight = fheight|0;
+                fwidth = fwidth|0, fheight = fheight|0;
 
                 const fontid = FontLib.lookup(font);
                 let width = 0, ascent = 0, descent = 0;
@@ -73,8 +75,8 @@
                 // Add in the effects of padding.  These are always set before the
                 // drawing constructor is called.
                 const padl = opts.paddingleft, padr = opts.paddingright, 
-                    padt = opts.paddingtop, padb = opts.paddingbottom, 
-                    rot = opts.rotate || 'N';
+                    padt = opts.paddingtop, padb = opts.paddingbottom; 
+                rot = opts.rotate || 'N';
 
                 width  += padl + padr;
                 height += padt + padb;
@@ -98,7 +100,7 @@
                 gs_dx = padl;
                 gs_dy = padt;
 
-                svg = '';
+                svg = [];
             },
             // Unconnected stroked lines are used to draw the bars in linear barcodes.
             // No line cap should be applied.  These lines are always orthogonal.
@@ -119,7 +121,7 @@
                 // Dramatically reduces resulting text size.
                 const key = `${lw}#${rgb}`;
                 if (!lines[key]) {
-                    lines[key] = `<path stroke="#${rgb}" stroke-width="${lw}" d="`;
+                    lines[key] = `<path class="not-text" stroke="#${rgb}" stroke-width="${lw}" d="`;
                 }
                 lines[key] += 'M' + transform(x0, y0) + 'L' + transform(x1, y1);
             },
@@ -129,11 +131,9 @@
             // You will see a series of polygon() calls, followed by a fill().
             polygon(pts) {
                 if (!path) {
-                    path = '<path d="';
+                    path = '<path class="not-text" d="';
                 }
-                path += 'M' + transform(pts[0][0], pts[0][1]);
-                path += pts.slice(1).map(([x,y])=>'L' + transform(x, y)).join('');
-                path += 'Z ';
+                path += 'M' + transform(pts[0][0], pts[0][1]) + pts.slice(1).map(([x,y])=>'L' + transform(x, y)).join('') + 'Z ';
             },
             // An unstroked, filled hexagon used by maxicode.  You can choose to fill
             // each individually, or wait for the final fill().
@@ -148,7 +148,7 @@
             // fill().
             ellipse(x, y, rx, ry, ccw) {
                 if (!path) {
-                    path = '<path d="';
+                    path = '<path class="not-text" d="';
                 }
                 const dx = rx * ELLIPSE_MAGIC, dy = ry * ELLIPSE_MAGIC;
 
@@ -163,7 +163,7 @@
             // PostScript's default fill rule is even-odd.
             fill(rgb) {
                 if (path) {
-                    svg += `${path}" fill="#${rgb}" fill-rule="evenodd" />\n`;
+                    svg.push(`${path}" fill="#${rgb}" fill-rule="evenodd" />`);
                     path = null;
                 }
             },
@@ -173,51 +173,105 @@
             // dx is extra space requested between characters (usually zero).
             text(x, y, str, rgb, font) {
                 const fontid  = FontLib.lookup(font.name);
-                const fwidth  = font.width|0, fheight = font.height|0;
-                const dx      = font.dx|0;
-                let path = '';
+                const fwidth  = (font.width * textScale.width)|0, fheight = (font.height * textScale.height)|0;
+                // const fwidth  = font.width|0, fheight = (font.height * textScale.height)|0;
+                const dx = (font.dx * textScale.width)|0;
+                // y = y - font.height + fheight;
+                // New algo uses relative positioning & glyph reuse via <def></def>
+                //  ┌--- Change this conditional to 'false' to revert back to absolute positioning & duplicate glyphs
+                //  v
+                const chars = new Set(str.split(""));
+                let glyph_href_map = new Map();
+                // Create the defs
                 for (let k = 0; k < str.length; k++) {
-                    const ch = str.charCodeAt(k);
-                    const glyph = FontLib.getpaths(fontid, ch, fwidth, fheight);
-                    if (!glyph) {
-                        continue;
-                    }
-                    if (glyph.length) {
+                    const charCode = str.charCodeAt(k)
+                    if (glyph_href_map.has(charCode)) { continue; }
+                    const glyph = FontLib.getpaths(fontid, charCode, font.width|0, font.height|0);
+                    if (!glyph) { continue; }
+                    if (glyph.length > 0) {
                         // A glyph is composed of sequence of curve and line segments.
                         // M is move-to
                         // L is line-to
                         // Q is quadratic bezier curve-to
                         // C is cubic bezier curve-to
-                        for (let i = 0, l = glyph.length; i < l; i++) {
-                            let seg = glyph[i];
-                            if (seg.type == 'M' || seg.type == 'L') {
-                                path += seg.type +  transform(seg.x  + x , y - seg.y  );
-                            } else if (seg.type == 'Q') {
-                                path += seg.type +  transform(seg.cx + x , y - seg.cy ) +
-                                                    transform(seg.x  + x , y - seg.y  );
-                            } else if (seg.type == 'C') {
-                                path += seg.type +  transform(seg.cx1 + x, y - seg.cy1) +
-                                                    transform(seg.cx2 + x, y - seg.cy2) +
-                                                    transform(seg.x   + x, y - seg.y  );
-                            }
+                        if (glyph[0].type !== 'M'){
+                            throw new Error("Text glyph doesn't start with 'M' command");
                         }
-                        // Close the shape
-                        path += 'Z ';
+                        
+                        const [initial_x, initial_y] = transformRaw(glyph[0].x + x, y - glyph[0].y)
+                        let prev_x = initial_x, prev_y = initial_y;
+                        const translate = ([x, y])=>`${x - prev_x},${y - prev_y} `;
+                        let path = [`${glyph[0].type}0,0 `];
+
+                        for (let i = 1; i < glyph.length; i++) {
+                            const seg = glyph[i];
+                            const new_coords = transformRaw(seg.x + x , y - seg.y);
+                            if (seg.type == 'M' || seg.type == 'L') {
+                                path.push(seg.type.toLowerCase() + translate(new_coords));
+                            } else if (seg.type == 'Q') {
+                                path.push('q' + translate(transformRaw(seg.cx + x , y - seg.cy)) + translate(new_coords));
+
+                            } else if (seg.type == 'C') {
+                                path.push('c' + translate(transformRaw(seg.cx1 + x, y - seg.cy1)) + 
+                                translate(transformRaw(seg.cx2 + x, y - seg.cy2)) + translate(new_coords));
+                            }
+                            [prev_x, prev_y] = new_coords;
+                        }
+                        // Close the shape, and push it to the array
+                        glyph_href_map.set(
+                            charCode, 
+                            `<g id="text_${charCode}"><path class="text" d="${path.join("")}Z"/></g>`
+                        );
                     }
+                }
+                for (const def of glyph_href_map.values()){
+                    glyph_defs.push(def);
+                }
+
+                let glyphs = [];
+                let start_x = undefined, start_y = undefined;
+                for (let k = 0; k < str.length; k++) {
+                    const glyph = FontLib.getpaths(fontid, str.charCodeAt(k), font.width|0, font.height|0);
+                    let initial_x = undefined, initial_y = undefined;
+                    if (!glyph) { continue; }
+                    if (glyph.length > 0) {
+                        if (glyph[0].type !== 'M'){
+                            throw new Error("Text glyph doesn't start with 'M' command");
+                        }
+                        [initial_x, initial_y] = transformRaw(glyph[0].x + x, y - glyph[0].y);
+                        if (start_x === undefined || start_y === undefined) {
+                            [start_x, start_y] = [initial_x, initial_y];
+                        }
+                    }
+                    if (initial_x === undefined || initial_y === undefined) {
+                        throw new Error("Glyph placement position cannot be found.");
+                    }
+                    glyphs.push(`<use href="#text_${str.charCodeAt(k)}" x="${initial_x - start_x}" y="${initial_y - start_y}" />`);
                     x += glyph.advance + dx;
                 }
-                if (path) {
-                    svg += `<path class="text" d="${path}" fill="#${rgb}" />\n`;
+                if (glyphs.length > 0) {
+                    const transforms = `translate(${start_x}, ${start_y})`
+                    + `translate(${textOffset.x}, ${textOffset.y})`
+                    + ( (rot === 'N' || rot === 'I') ?
+                        `scale(${textScale.width}, ${textScale.height}) ` :
+                        `scale(${textScale.height}, ${textScale.width}) `
+                    );
+                    svg.push(`<g class="text" transform="${transforms}">\n\t\t${glyphs.join("\n\t\t")}\n\t</g>`);
                 }
             },
             // Called after all drawing is complete.  The return value from this method
             // is the return value from `bwipjs.render()`.
             end() {
-                let linesvg = Object.values(lines).map(l=>`${l}" />`).join('\n');
-                let bg = opts.backgroundcolor;
-                return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="${gs_width}" height="${gs_height}" viewBox="0 0 ${gs_width} ${gs_height}" preserveAspectRatio="xMidYMid meet">\n
-                ${(/^[0-9A-Fa-f]{6}$/.test(''+bg) ? `<rect id="barcodeBG" width="100%" height="100%" fill="#${bg}" />\n` : '')}
-                ${linesvg}${svg}\n</svg>`;
+                const linesvg = Object.values(lines).map(l=>`${l}" />`).join('\n\t\t');
+                const bg = opts.backgroundcolor;
+                return [
+                    `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="${gs_width}" height="${gs_height}" viewBox="0 0 ${gs_width} ${gs_height}" preserveAspectRatio="xMidYMid meet">`,
+                    `\t<def>${glyph_defs.join("\n\t\t")}</def>`,
+                    /^[0-9A-Fa-f]{6}$/.test(''+bg) ? `\t<rect id="barcodeBG" width="100%" height="100%" fill="#${bg}" />` : '',
+                    `\t<g id="linesArea">\n\t\t${linesvg}\n\t</g>`,
+                    `\t${svg.join("\n\t")}`,
+                    '</svg>'
+                ].join("\n")
             },
         };
 
@@ -230,6 +284,14 @@
             let tx = tx0 * x + tx1 * y + tx2 * (gs_width-1) + tx3 * (gs_height-1);
             let ty = ty0 * x + ty1 * y + ty2 * (gs_width-1) + ty3 * (gs_height-1);
             return `${integers_only ? Math.round(tx) : roundTo2DP(tx)},${integers_only ? Math.round(ty) : roundTo2DP(ty)} `;
+        }
+
+        function transformRaw(x, y) {
+            x += gs_dx;
+            y += gs_dy;
+            let tx = tx0 * x + tx1 * y + tx2 * (gs_width-1) + tx3 * (gs_height-1);
+            let ty = ty0 * x + ty1 * y + ty2 * (gs_width-1) + ty3 * (gs_height-1);
+            return integers_only ? [Math.round(tx), Math.round(ty)] : [roundTo2DP(tx), roundTo2DP(ty)];
         }
     }
 
